450bbe636f1f2a9c3495cb4054319a72a3475045
Unknown <lejeune.q@hotmail.com>
2020-05-13 09:52:17 +0200

    singleton fixe

--- src/dataAccess/DBConnection.java
+++ src/dataAccess/DBConnection.java
@@ -3,26 +3,28 @@ package dataAccess;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
     private static Connection connection = null;
 
-    // Modify to suit your config
+    // Modify to suit your settings
     private static String url = "jdbc:mysql://localhost:3306/beeritsimple";
     private static String id = "root";
     private static String password = "root";
 
     private DBConnection() {
+        try {
+            connection = DriverManager.getConnection(url, id, password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
+    // TODO: remplacer par getinstance
     public static Connection getDBConnection() {
-        try {
-            if (connection == null) {
-                connection = DriverManager.getConnection(url, id, password);
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        if (connection == null)
+            new DBConnection();
 
         return connection;
     }
